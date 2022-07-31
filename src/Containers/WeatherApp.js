f0a9808206460a3d128cdfd3f41ed5dc8b9e238e
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-07-31 21:46:12 +0100

    api integrated

--- src/Containers/WeatherApp.js
+++ src/Containers/WeatherApp.js
@@ -1,43 +1,77 @@
 import SearchEngine from "../Components/SearchEngine";
 import "./WeatherApp.css";
 import axios from "axios";
+import { useState } from "react";
 
 function WeatherApp() {
-  const apiKey = "d8426e0d7454e83e722791e94527aed3";
-  let city = "Edinburgh";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric
-    `;
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-  return (
-    <div className="WeatherApp">
-      <SearchEngine />
-      <h1>Edinburgh</h1>
-      <ul>
-        <li>Wednesday 07:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="d-flex">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Mostly Cloudy"
-              className="d-flex"
-            />
-            <span className="temperature">18</span>
-            <span className="unit">°C</span>
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      icon: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      date: "Wednesday 07:00",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="WeatherApp">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="d-flex">
+              <img
+                src={weatherData.icon}
+                alt={weatherData.description}
+                className="d-flex"
+              />
+              <div className="d-flex">
+                <span className="temperature">
+                  {Math.round(weatherData.temperature)}
+                </span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {Math.round(weatherData.wind)} km/h</li>
+            </ul>
           </div>
         </div>
-        <div className="col-6">
-          <ul>
-            <li>Preciptation: 5%</li>
-            <li>Humidity: 62%</li>
-            <li>Wind: 9 km/h</li>
-          </ul>
-        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "d8426e0d7454e83e722791e94527aed3";
+    let city = "Edinburgh";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric
+    `;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
 
 export default WeatherApp;
